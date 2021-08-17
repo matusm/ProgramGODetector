84bc5c920ff985d1b18a53f208706d867d0078b6
Michael Matus <michael@matus.at>
2021-08-17 15:54:06 +0200

    Check if modifications requested. Otherwise abort.

--- Program.cs
+++ Program.cs
@@ -49,38 +49,46 @@ namespace ProgramGODetector
             LogAndDisplay(memoryDump);
             LogOnly(thinSeparator);
 
-            LogAndDisplay("Requested modification:");
-            if (newSetting.CalibrationFactor is double cf)
-                LogAndDisplay($"Calibration factor: {cf}");
-            if (newSetting.PhotometricUnit is int unit)
-                LogAndDisplay($"Photometric unit: {unit} -> {newSetting.PhotometricUnitSymbol}");
-            if (newSetting.SerialNumber is int sn)
-                LogAndDisplay($"Serial number: {sn}");
-            if (newSetting.DetectorName is string dn)
-                LogAndDisplay($"Detector name: {dn}");
-            if (newSetting.CustomString is string cs)
-                LogAndDisplay($"Custom string: {cs}");
-
-
-            DisplayOnly("Are you sure to modify the detector settings?");
-            DisplayOnly("press 'y' to modify detector settings - any other key to quit");
-
-            if (Console.ReadKey(true).Key == ConsoleKey.Y)
+            if (newSetting.IsNullOrEmpty)
             {
-                device.WriteDetectorStatusToRam(newSetting);
-                device.SaveRamToEeprom();
-                LogAndDisplay("Reprogramming of detector successful");
+                // no modifications requested via CLI
+                LogAndDisplay("No modification requested - aborting.");
+            }
+            else
+            {
+                // modifications requested via CLI
+                LogAndDisplay("Requested modification:");
+                if (newSetting.CalibrationFactor is double cf)
+                    LogAndDisplay($"Calibration factor: {cf}");
+                if (newSetting.PhotometricUnit is int unit)
+                    LogAndDisplay($"Photometric unit: {unit} -> {newSetting.PhotometricUnitSymbol}");
+                if (newSetting.SerialNumber is int sn)
+                    LogAndDisplay($"Serial number: {sn}");
+                if (newSetting.DetectorName is string dn)
+                    LogAndDisplay($"Detector name: {dn}");
+                if (newSetting.CustomString is string cs)
+                    LogAndDisplay($"Custom string: {cs}");
+                DisplayOnly("Are you sure to modify the detector settings?");
+                DisplayOnly("press 'y' to modify detector settings - any other key to quit");
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Y)
+                {
+                    device.WriteDetectorStatusToRam(newSetting);
+                    device.SaveRamToEeprom();
+                    LogAndDisplay("Reprogramming of detector successful.");
+                }
+                else
+                {
+                    LogAndDisplay("Reprogramming of detector aborted.");
+                }
             }
             timeStamp = DateTime.UtcNow;
             DisplayOnly("bye.");
             LogOnly("");
             LogOnly(fatSeparator);
-                LogOnly($"StopTimeUTC: {timeStamp:dd-MM-yyyy HH:mm}");
+            LogOnly($"StopTimeUTC: {timeStamp:dd-MM-yyyy HH:mm}");
             LogOnly(fatSeparator);
             LogOnly("");
-
-
-
             streamWriter.Close();
 
             /***************************************************/
